26d4e19e2260c6182a24375c384e835b1aad83cc
Gurmeet Saran <gurmeetx@gmail.com>
2025-06-17 21:32:34 -0700

    add mocking

--- src/db_types/__init__.py
+++ src/db_types/__init__.py
@@ -50,6 +50,42 @@ from db_types.types.numeric import (
 from db_types.types.string import CHAR, TEXT, VARCHAR
 from db_types.types.temporal import DATE, DATETIME, TIME, TIMESTAMP
 
+# Import mock utilities
+try:
+    from db_types.decorators import mockable  # noqa: F401
+    from db_types.mock_builder import MockBuilder  # noqa: F401
+    from db_types.mock_factory import mock_factory  # noqa: F401
+
+    _mock_exports = ["mockable", "MockBuilder", "mock_factory"]
+except ImportError:
+    # Mock utilities require faker
+    _mock_exports = []
+
+# Import specialized types
+try:
+    from db_types.specialized import (  # noqa: F401
+        URL,
+        City,
+        Country,
+        Email,
+        PhoneNumber,
+        State,
+        ZipCode,
+    )
+
+    _specialized_types = [
+        "City",
+        "Country",
+        "Email",
+        "PhoneNumber",
+        "State",
+        "URL",
+        "ZipCode",
+    ]
+except ImportError:
+    # Specialized types may not be available in all environments
+    _specialized_types = []
+
 __all__ = [
     "BIGINT",
     "BINARY",
@@ -101,6 +137,8 @@ __all__ = [
     "TinyInt",
     "VarBinary",
     "Varchar",
+    *_specialized_types,
+    *_mock_exports,
 ]
 
 __version__ = "0.1.0"
